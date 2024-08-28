2c2d0a08c91ab1a8d5a4515d421537b38e435b66
Antonio <antonio.moreira@ugba.br>
2024-08-28 17:40:39 -0300

    lex

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,52 @@
-/* main.c */
+#include <stdio.h>
+#include <string.h>
+#include "e2.tab.h"  // Arquivo gerado pelo Flex com as definições de token
 
+extern int yylex();
+extern int yylval;
+
+void yyrestart(FILE *input_file);
+
+int main() {
+    int token;
+    char input[1024];  // Buffer para armazenar a entrada
+
+    printf("Digite uma expressão aritmética (ou CTRL+D para sair):\n");
+
+    while (fgets(input, sizeof(input), stdin) != NULL) {
+        // Reinicia o analisador léxico para ler a nova linha
+        yy_scan_string(input);
+
+        // Processa os tokens da linha
+        while ((token = yylex()) != 0) {
+            switch (token) {
+                case NUMBER:
+                    printf("Número: %d\n", yylval);
+                    break;
+                case PLUS:
+                    printf("Operador: +\n");
+                    break;
+                case MINUS:
+                    printf("Operador: -\n");
+                    break;
+                case MULT:
+                    printf("Operador: *\n");
+                    break;
+                case DIV:
+                    printf("Operador: /\n");
+                    break;
+                case ERROR:
+                    printf("Erro léxico: %s\n", yytext);
+                    break;
+                default:
+                    printf("Token desconhecido: %d\n", token);
+                    break;
+            }
+        }
+
+        printf("Digite outra expressão aritmética (ou CTRL+D para sair):\n");
+    }
+
+    return 0;
+}
+}
